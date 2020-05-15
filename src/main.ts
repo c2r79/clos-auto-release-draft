84697b9d2dcfcf62a45e70fd2235657156f9e819
c2r79 <carlos.r.romero7@gmail.com>
2020-05-15 08:34:44 -0400

    adding 1st action, not updating security vulnerability

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // * We return the release-url parameter
+    core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     core.setFailed(error.message)
   }
 }
 
-run()
+export default run
