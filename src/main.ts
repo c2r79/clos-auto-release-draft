7376f4af3c96a862b13de203c6c11abdb9ed69b5
c2r79 <carlos.r.romero7@gmail.com>
2020-05-19 14:42:02 -0400

    returned to a running state

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,7 @@ async function run(): Promise<void> {
     if (tag && version.isSemVer(tag)) {
       const changeLog = await git.getChangesIntroducedByTag(tag)
       core.debug(`Detected changelog:\n${changeLog}`)
-      // releaseUrl = await github.createReleaseDraft(tag, token, changeLog)
-      releaseUrl = await github.createReleaseDraft('wrong', token, changeLog)
+      releaseUrl = await github.createReleaseDraft(tag, token, changeLog)
     }
     // * We return the release-url parameter
     core.setOutput('release-url', releaseUrl)
